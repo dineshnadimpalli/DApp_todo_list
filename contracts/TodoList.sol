32a156015e99d1ff8f15eab5d1155524456c00aa
Dinesh Nadimpalli <dinesh601048@gmail.com>
2021-05-14 15:13:30 +0530

    Created tasks and tests

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,28 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    mapping (uint=>Task) public tasks;
+
+    constructor() public {
+        createTask(("Learn to build DApps"));
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
 }
